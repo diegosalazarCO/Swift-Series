67783598032671d57c320e23847fbd8e82ad292b
Diego Salazar <kirafreaky@gmail.com>
2015-12-29 23:10:34 -0300

    Added total per person function

--- 04. iOS 101/Parte 3 - Principales Controles de UIKit/TipCalc/TipCalc.swift
+++ 04. iOS 101/Parte 3 - Principales Controles de UIKit/TipCalc/TipCalc.swift
@@ -10,9 +10,12 @@ import Foundation
 
 class TipCalculator {
     
+    var total: Float = 0
+    var totalPerPerson: Float = 0
     var tipAmount: Float = 0
     var amountBeforeTax: Float = 0
     var tipPercentage: Float = 0
+    var people:Int = 1
 
 
     init(amountBeforeTax: Float, tipPercentage: Float) {
@@ -23,5 +26,13 @@ class TipCalculator {
 
     func calculateTip() {
         tipAmount = amountBeforeTax * tipPercentage
+    }
+    
+    func calculateTotalPerPerson() {
+        totalPerPerson = total / Float(people)
+    }
+    
+    func calculateTotal() {
+        total = amountBeforeTax + tipAmount
     }
 }
